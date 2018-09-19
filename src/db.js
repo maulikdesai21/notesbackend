d84bdde6f479269cd29a4d4ae1846adac0263161
Desai, Maulik (CORP) <Maulik.Desai@adp.com>
2018-09-19 17:22:22 -0400

    Adding MongoDB Connection Settings

--- src/db.js
+++ src/db.js
@@ -1,4 +1,39 @@
+import chalk from "chalk";
+import mongoose from "mongoose";
+import {dbUrl} from "./config.json";
+
+const connected = chalk.bold.cyan;
+const error = chalk.bold.yellow;
+const disconnected = chalk.bold.red;
+const termination = chalk.bold.magenta;
 export default callback => {
-	// connect to a database if needed, then pass it to `callback`:
-	callback();
-}
+  // connect to a database if needed, then pass it to `callback`:
+  console.log('I am called')
+  mongoose.connect(dbUrl);
+
+  mongoose.connection.on("connected", function() {
+    console.log(connected("Mongoose default connection is open to ", dbUrl));
+  });
+  mongoose.connection.on("error", function(err) {
+    console.log(
+      error("Mongoose default connection has occured " + err + " error")
+    );
+  });
+
+  mongoose.connection.on("disconnected", function() {
+    console.log(disconnected("Mongoose default connection is disconnected"));
+  });
+
+  process.on("SIGINT", function() {
+    mongoose.connection.close(function() {
+      console.log(
+        termination(
+          "Mongoose default connection is disconnected due to application termination"
+        )
+      );
+      process.exit(0);
+    });
+  });
+
+  callback();
+};
--- src/index.js
+++ src/index.js
@@ -24,13 +24,13 @@ app.use(bodyParser.json({
 }));
 
 // connect to db
-initializeDb( db => {
+initializeDb( () => {
 
 	// internal middleware
-	app.use(middleware({ config, db }));
+	app.use(middleware({ config }));
 
 	// api router
-	app.use('/api', api({ config, db }));
+	app.use('/api', api({ config }));
 
 	app.server.listen(process.env.PORT || config.port, () => {
 		console.log(`Started on port ${app.server.address().port}`);
